2a6cd52ae7b93ed6092e2ea5e61e0fdd9988d084
khalilalmasri <96783293+khalilalmasri@users.noreply.github.com>
2024-10-16 12:47:16 +0300

    Add CRUD operations for products in NestJS controller
    - Created `CreateProductDto` and `UpdateProductDto` classes in the `dtos` directory to define the structure of product data for creation and updates.
    - Updated the `ProductsController` to include CRUD operations:
      - `POST /api/products` to create a new product.
      - `GET /api/products` to retrieve all products.
      - `GET /api/products/:id` to retrieve a single product by ID.
      - `PUT /api/products/:id` to update a product by ID.
      - `DELETE /api/products/:id` to delete a product by ID.
    - Added a `products` array in the `ProductsController` to simulate a database for storing product data.
    - Configured VSCode settings to recognize the custom word "dtos" for spell checking.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,79 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+  Put,
+  Delete, // BadRequestException,UnauthorizedException,ForbiddenException,
+} from '@nestjs/common';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
+type ProductType = { id: number; title: string; price: number };
 
-@Controller()
+@Controller('api/products')
 export class ProductsController {
-  @Get('/api/products')
+  private products: ProductType[] = [
+    { id: 1, title: 'book', price: 10 },
+    { id: 2, title: 'pen', price: 5 },
+    { id: 3, title: 'laptop', price: 2 },
+  ];
+
+  // Post: POST /api/products
+  @Post()
+  public createProduct(@Body() body: CreateProductDto) {
+    const newProduct: ProductType = {
+      id: this.products.length + 1,
+      title: body.title,
+      price: body.price,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  // Get: GET /api/products
+  @Get()
   public getAllProducts() {
-    return [
-      { id: 1, title: 'book', price: 10 },
-      { id: 2, title: 'pen', price: 5 },
-      { id: 3, title: 'laptop', price: 2 },
-    ];
+    return this.products;
+  }
+
+  // Get: GET /api/products/:id
+  @Get('/:id')
+  public getSingleProduct(@Param('id') id: string) {
+    const product = this.products.find((p) => p.id === +id);
+    if (!product) {
+      throw new NotFoundException("product doesn't exist", {
+        description: 'Product not found', // optional
+      });
+    }
+    return product;
+  }
+
+  // Put: PUT /api/products/:id
+  @Put('/:id')
+  public updateProduct(
+    @Param('id') id: string,
+    @Body() body: UpdateProductDto,
+  ) {
+    const product = this.products.find((p) => p.id === +id);
+    if (!product) {
+      throw new NotFoundException("product doesn't exist", {
+        description: 'Product not found', // optional
+      });
+    }
+    console.log(body);
+    return { message: 'update success' + id };
+  }
+
+  @Delete('/:id')
+  public deleteProduct(@Param('id') id: string) {
+    const product = this.products.find((p) => p.id === +id);
+    if (!product) {
+      throw new NotFoundException("product doesn't exist", {
+        description: 'Product not found', // optional
+      });
+    }
+    return { message: 'delete success' + id };
   }
 }
