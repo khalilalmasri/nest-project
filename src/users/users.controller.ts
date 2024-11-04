f6578d82c0372979b1c4ed73ea1e3b848584eede
khalilalmasri <96783293+khalilalmasri@users.noreply.github.com>
2024-11-04 16:34:24 +0200

    Add user login functionality
    - Created a new `LoginDto` class in `src/users/Dtos/login.dto.ts` to validate login request data.
    - Added a `login` method to `UsersController` in `src/users/users.controller.ts` to handle POST requests to the `/api/users/auth/login` endpoint.
    - Implemented the `login` method in `UsersService` in `src/users/users.service.ts` to authenticate users by email and password, and return the user object if successful.
    - Updated the `users.http` file to include a new request for user login.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { RegisterDto } from './Dtos/register.dto';
+import { LoginDto } from './Dtos/login.dto';
 
 @Controller('api/users')
 export class UsersController {
@@ -8,5 +9,11 @@ export class UsersController {
   @Post('auth/register')
   public register(@Body() body: RegisterDto) {
     return this.usersService.register(body);
+  }
+
+  @Post('auth/login')
+  @HttpCode(HttpStatus.OK) // to set status code == 200 by default is 201
+  public login(@Body() body: LoginDto) {
+    return this.usersService.login(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './Dtos/login.dto';
 
 @Injectable()
 export class UsersService {
@@ -34,5 +35,29 @@ export class UsersService {
     newUser = await this.usersRepository.save(newUser);
     // todo -> generate jwt token
     return newUser;
+  }
+
+  /**
+   * login user
+   * @param loginDto data for login
+   * @returns user object  --> jwt token
+   * @throws BadRequestException if user not found or invalid username or password
+   */
+  public async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const userFromDB = await this.usersRepository.findOne({
+      where: { email },
+    });
+    if (!userFromDB)
+      throw new BadRequestException(
+        'user not found || invalid username or password',
+      );
+    const isPasswordMatch = await bcrypt.compare(password, userFromDB.password);
+    if (!isPasswordMatch)
+      throw new BadRequestException(
+        'wrong password || invalid username or password',
+      );
+    // to do -> generate jwt token
+    return userFromDB;
   }
 }
